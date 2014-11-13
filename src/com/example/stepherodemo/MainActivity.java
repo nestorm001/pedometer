63753a3596b985947ce419b774c76b4f246c1cbe
nestorm001 <nestorm001@gmail.com>
2014-11-13 11:00:07 +0800

    ok

--- src/com/example/stepherodemo/MainActivity.java
+++ src/com/example/stepherodemo/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.stepherodemo;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -13,29 +16,46 @@ public class MainActivity extends Activity {
 	int count = 0;
 	TextView tv;
 	Button b;
+	ProgressBar progressBar = null;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		tv = (TextView) findViewById(R.id.textView);
+		progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
 		b = (Button) findViewById(R.id.button);
 		b.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				count++;
-				if(count > 3){
+				if (count > 3) {
 					System.exit(0);
 				}
 			}
 		});
-				
-		StepListener sl = new StepListener(this);
+		
+
+		final StepListener sl = new StepListener(this);
 		sl.setOnStepListener(new StepListener.OnStepListener() {
 			@Override
 			public void onStep() {
 				step++;
 				tv.setText("当前走了：" + step + "步");
+				int progressBarMax = progressBar.getMax();
+				int stepProgress = step * progressBarMax / 100;
+				Log.d("step", "" + progressBarMax);
+				if (progressBarMax != progressBar.getProgress()) {
+					progressBar.setProgress(stepProgress);
+				} else {
+					new AlertDialog.Builder(MainActivity.this)
+					.setTitle("Message")
+					.setMessage("恭喜你到达了小村庄！！")
+					.setPositiveButton("确定", null).show();
+					sl.pause();
+				}
 			}
 		});
 	}
